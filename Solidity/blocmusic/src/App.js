70154b28a7c63ee63799b7c80f7958d92a25a4b2
Michael <gadreelazazel@gmail.com>
2022-11-21 14:01:32 +0100

    Update App.js

--- Solidity/blocmusic/src/App.js
+++ Solidity/blocmusic/src/App.js
@@ -1,31 +1,45 @@
 import React from 'react';
+import { useState } from 'react';
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import Album from './pages/Album';
 import './App.css';
 import { Layout } from 'antd';
 import logo from './logo.svg';
+import Spotify from "./images/Spotify.png";
+import Player from "./components/AudioPlayer";
 import { SearchOutlined, DownCircleOutlined } from "@ant-design/icons"; 
 
 const { Header, Footer, Content } = Layout;
 
 
 const App = () =>{
+  
+  const [nftAlbum, setNftAlbum] = useState();
   return (
-    <Layout>
-      <Header style={{
-        position: 'sticky',
-        top: 0,
-        zIndex: 1,
-        width: '100%',
-      }}>Header</Header>
-      <Content className="contentWindow">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/album" element={<Album />} />
-          </Routes>
-        </Content>
-    </Layout>
+      <Layout>
+        <Header style={{
+          position: 'sticky',
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+        }}><div className="logo" />
+        <Link to="/"><img src={Spotify} style={{height:40, width: 40}} alt="Logo" className="logo"></img></Link>
+        </Header>
+        <Content className="contentWindow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/album" element={<Album />} />
+            </Routes>
+          </Content>
+          <Footer className="footer">
+          {nftAlbum &&
+          <Player
+            url={nftAlbum}
+          />
+          }
+        </Footer>
+      </Layout>
   );
 }
 
