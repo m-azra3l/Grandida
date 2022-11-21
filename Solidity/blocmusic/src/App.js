f13571f97f561bce6273cd39903bf475d2a92038
Michael <gadreelazazel@gmail.com>
2022-11-21 11:04:37 +0100

    Update App.js

--- Solidity/blocmusic/src/App.js
+++ Solidity/blocmusic/src/App.js
@@ -1,26 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Album from './pages/Album';
 import './App.css';
 import { Layout } from 'antd';
+import logo from './logo.svg';
+import { SearchOutlined, DownCircleOutlined } from "@ant-design/icons"; 
+
 const { Header, Footer, Content } = Layout;
 
-function App() {
+
+const App = () =>{
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header style={{
+        position: 'sticky',
+        top: 0,
+        zIndex: 1,
+        width: '100%',
+      }}>Header</Header>
+      <Content className="contentWindow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/album" element={<Album />} />
+          </Routes>
+        </Content>
+    </Layout>
   );
 }
 
