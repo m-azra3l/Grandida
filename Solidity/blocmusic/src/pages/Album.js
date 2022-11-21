b54dc20cacfa9349353400df86a210a59e6fd271
Michael <gadreelazazel@gmail.com>
2022-11-21 14:01:40 +0100

    Update Album.js

--- Solidity/blocmusic/src/pages/Album.js
+++ Solidity/blocmusic/src/pages/Album.js
@@ -8,9 +8,31 @@ import { ClockCircleOutlined } from "@ant-design/icons";
 const Album = () => {
   
     const {state: album} = useLocation();
+
+    /*const albumDetails = [
+        {
+            "token_address": "0x8a68d4e28515815cd6026416f4b2a4b647796f3e",
+            "token_id": "4",
+            "amount": "1",
+            "contract_type": "ERC721",
+            "name": "Shadow",
+            "symbol": "shad",
+        }
+    ]*/
     return (
     <>
-    <button onClick={() =>console.log(album)}> HIIII </button>
+    <div className='albumContent'>
+        <div className='topBan'>
+            <img
+                src={album.image}
+                alt="albumcover"
+                className="albumCover"></img>
+            <div className='albumDeets'>
+                <div>Album</div>
+                <div className='title'>{album.title}</div>
+            </div>
+        </div>
+    </div>
     </>
   )
   }
